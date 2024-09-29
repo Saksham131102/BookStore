49680847253576970ba7316ad38e6842cee515a8
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-09-29 15:27:55 +0530

    logout functionality added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,16 +2,20 @@ import { ShoppingCart } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useAuthContext } from "@/context/authContext";
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import useLogout from "@/hooks/useLogout";
+import { ThreeDots } from "react-loader-spinner";
 
 const Navbar = () => {
   const { authUser } = useAuthContext();
+  const { loading, logout } = useLogout();
 
   return (
     <div className="flex justify-between items-center px-40 py-4 font-poppins sticky top-0 w-full bg-[#ababab]">
       <Link to="/" className="text-xl font-semibold">
         Book Store
       </Link>
-      <div className="flex">
+      <div className="flex items-center">
         <Link
           to="/library"
           className="flex gap-1 items-center transition-all duration-200 hover:bg-slate-300 rounded-full cursor-pointer py-2 px-4"
@@ -27,6 +31,21 @@ const Navbar = () => {
           <AvatarImage src={authUser?.profilePic} alt="@shadcn" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
+        <Button onClick={logout} className="ml-3 w-20">
+          {loading ? (
+            <ThreeDots
+              height="30"
+              width="30"
+              radius="9"
+              color="white"
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{}}
+              visible={true}
+            />
+          ) : (
+            "Logout"
+          )}
+        </Button>
       </div>
     </div>
   );
